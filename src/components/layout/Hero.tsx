ac92bee2c16c1c85bbb30c6be30f01861a4a1721
Raka Grarizki Arsetyo <raka.arsetyo@bfi.co.id>
2023-05-31 13:38:44 +0700

    feat: new hero component, for skeleton before unload image

---
+++ src/components/layout/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Skeleton from '@/components/Skeleton';
+
+export default function Hero() {
+  return (
+    <div>
+      <Skeleton className='h-96 w-auto' />
+    </div>
+  );
+}
